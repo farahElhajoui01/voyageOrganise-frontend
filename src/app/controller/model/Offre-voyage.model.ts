98746730afd03e9bbe02c42fc93f0f069b5eafc4
EL HAJOUI Farah <farahelhajoui01@gmail.com>
2020-12-09 14:45:20 +0100

    offerDetails page+controller

---
+++ src/app/controller/model/Offre-voyage.model.ts
@@ -0,0 +1,17 @@
+import {Choix} from './Choix.model';
+import {Commentaire} from './Commentaire.model';
+
+export class OffreVoyage {
+
+  public id: number;
+  public description: string;
+  public programme: string;
+  public rating: number;
+  public dateCreation: Date;
+  public choix = new Array<Choix>();
+  // public agence: Agence;
+//  public typeVoyage :TypeVoyage;
+  public commentaires = new Array<Commentaire>();
+  //public parcours = new Array<Parcour>();
+
+}
