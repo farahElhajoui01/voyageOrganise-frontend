bffaef39f52523efe5ad379214836812fbeece5b
EL HAJOUI Farah <farahelhajoui01@gmail.com>
2020-12-09 15:38:27 +0100

    offerDetails page+controller+some models

--- src/app/controller/model/Commentaire.model.ts
+++ src/app/controller/model/Commentaire.model.ts
@@ -1,5 +1,10 @@
+import { OffreVoyage } from './Offre-voyage.model';
 export class Commentaire {
 
-
+	private  id:Number;
+	private  libelle:String;
+    //private  voyageur:Voyageur;
+    private  offreVoyage:OffreVoyage;
+	
 }
 
